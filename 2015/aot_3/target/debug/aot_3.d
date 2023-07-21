d22f9663e452566d004d1fdccd1754d620cd74f4
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2023-07-21 11:02:11 -0300

    aoc_4

--- 2015/aot_3/target/debug/aot_3.d
+++
@@ -1,1 +0,0 @@
-/home/gabriel/Documents/code/adventure-of-code/2015/aot_3/target/debug/aot_3: /home/gabriel/Documents/code/adventure-of-code/2015/aot_3/src/main.rs
--- 2015/aot_3/target/debug/deps/aot_3-c8ed296236eff5b9.d
+++
@@ -1,5 +0,0 @@
-/home/gabriel/Documents/code/adventure-of-code/2015/aot_3/target/debug/deps/aot_3-c8ed296236eff5b9.rmeta: src/main.rs
-
-/home/gabriel/Documents/code/adventure-of-code/2015/aot_3/target/debug/deps/aot_3-c8ed296236eff5b9.d: src/main.rs
-
-src/main.rs:
